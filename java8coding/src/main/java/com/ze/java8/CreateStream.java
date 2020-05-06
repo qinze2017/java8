f9726a6d3bb9ee641eceb5a8473a7f847c6dec9d
qinze2017 <qinze2002@126.com>
2020-05-06 13:55:11 -0400

    updates

--- java8coding/src/main/java/com/ze/java8/CreateStream.java
+++ java8coding/src/main/java/com/ze/java8/CreateStream.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
@@ -21,7 +23,12 @@ public class CreateStream {
         /*createStreamFromCollection().forEach(System.out::println);
         createStreamFromValues().forEach(System.out::println);*/
         //createStreamFromArrays().forEach(System.out::println);
-        Stream<String> streamFromFile = createStreamFromFile();
+
+        //Stream<String> streamFromFile = createStreamFromFile();
+
+        //createStreamFromIterator().forEach(System.out::println);
+        //createStreamFromGenerator().forEach(System.out::println);
+        createObjStreamFromGenerator().forEach(System.out::println);
     }
 
     private static  Stream<String> createStreamFromCollection(){
@@ -45,6 +52,64 @@ public class CreateStream {
             return streamFromFile;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static Stream<Integer> createStreamFromIterator() {
+        Stream<Integer> stream = Stream.iterate(0, n -> n + 2).limit(10);
+        return stream;
+    }
+
+    private static Stream<Double> createStreamFromGenerator() {
+        return Stream.generate(Math::random).limit(10);
+    }
+
+    private static  Stream<Obj> createObjStreamFromGenerator() {
+        return Stream.generate(new ObjSupplier()).limit(10);
+    }
+
+    static class ObjSupplier implements Supplier<Obj> {
+        private int index = 0;
+        private Random random = new Random(System.currentTimeMillis());
+
+        @Override
+        public Obj get() {
+            index = random.nextInt(100);
+            return new Obj(index, "Name ->" + index);
+        }
+    }
+
+    static class Obj {
+        private int id;
+        private String name;
+
+        public Obj(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Obj{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
         }
     }
 }
