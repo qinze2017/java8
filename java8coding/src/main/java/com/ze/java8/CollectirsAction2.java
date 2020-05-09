4ea96facb2364a1a36a3f9453b2e9024e8820668
qinze2017 <qinze2002@126.com>
2020-05-09 17:48:53 -0400

    updates

--- java8coding/src/main/java/com/ze/java8/CollectirsAction2.java
+++ java8coding/src/main/java/com/ze/java8/CollectirsAction2.java
@@ -1,10 +1,10 @@
 package com.ze.java8;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 // passive loading class without initialize, if remove final, so it will be active loading
@@ -28,6 +28,28 @@ public class CollectirsAction2 {
         testMapping();
         testMaxBy();
         testMinBy();
+
+        testPartitioningByWithPredicate();
+        testPartitioningByWithPredicateAndCollector();
+        testReducingBinaryOperator();
+        testReducingBinaryOperatorAndIdentity();
+        testReducingBinaryOperatorAndIdentityAndFunction();
+        testSummarizingDouble();
+        testSummarizingInt();
+        testSummarizingLong();
+
+        testSummeringDouble();
+        testSummeringLong();
+        testSummeringInt();
+        testToCollection();
+        testToConcurrentMap();
+        testToConcurrentMapWithBinaryOperator();
+        testToConcurrentMapWithBinaryOperatorAndSupplier();
+        testToList();
+        testToSet();
+        testToMap();
+        testToMapWithBinaryOperator();
+        testToMapWithBinaryOperatorAndSupplier();
     }
 
     private static void testGroupingByConcurrentWithFunction(){
@@ -74,5 +96,164 @@ public class CollectirsAction2 {
     private static void testMinBy(){
         menu.stream().collect(Collectors.minBy(Comparator.comparing(Dish::getCalories)))
                 .ifPresent(System.out::println);
+    }
+
+    private static void testPartitioningByWithPredicate(){
+        Map<Boolean, List<Dish>> collect = menu.stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
+        Optional.of(collect).ifPresent(System.out::println);
+    }
+
+    private static void testPartitioningByWithPredicateAndCollector(){
+        Map<Boolean, Double> collect = menu.stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian, Collectors.averagingInt(Dish::getCalories)));
+        Optional.of(collect).ifPresent(System.out::println);
+    }
+
+    private static void testReducingBinaryOperator(){
+        menu.stream()
+                .collect(
+                        Collectors.reducing(
+                                BinaryOperator.maxBy(
+                                        Comparator.comparing(
+                                                Dish::getCalories)
+                                )
+                        )
+                ).ifPresent(System.out::println);
+    }
+
+    private static void testReducingBinaryOperatorAndIdentity(){
+        Integer collect = menu.stream()
+                .map(Dish::getCalories)
+                .collect(Collectors.reducing(0, (d1, d2) -> d1 + d2));
+        System.out.println(collect);
+    }
+
+    private static void testReducingBinaryOperatorAndIdentityAndFunction(){
+        Integer collect = menu.stream()
+                .collect(Collectors.reducing(0, Dish::getCalories, (d1, d2) -> d1 + d2));
+        System.out.println(collect);
+    }
+
+    private static void testSummarizingDouble(){
+        Optional.of(menu.stream().collect(Collectors.summarizingDouble(Dish::getCalories)))
+                .ifPresent(System.out::println);
+    }
+
+    private static void testSummarizingInt(){
+        Optional.of(menu.stream().collect(Collectors.summarizingInt(Dish::getCalories)))
+                .ifPresent(System.out::println);
+    }
+
+    private static void testSummarizingLong(){
+        Optional.of(menu.stream().collect(Collectors.summarizingLong(Dish::getCalories)))
+                .ifPresent(System.out::println);
+    }
+
+    private static void testSummeringDouble(){
+        Optional.of(menu.stream().collect(Collectors.summingDouble(Dish::getCalories)))
+                .ifPresent(System.out::println);
+
+        Optional.of(menu.stream().map(Dish::getCalories).mapToInt(Integer::intValue).sum())
+                .ifPresent(System.out::println);
+
+    }
+
+    private static void testSummeringLong(){
+        Optional.of(menu.stream().collect(Collectors.summingLong(Dish::getCalories)))
+                .ifPresent(System.out::println);
+    }
+
+    private static void testSummeringInt(){
+        Optional.of(menu.stream().collect(Collectors.summingInt(Dish::getCalories)))
+                .ifPresent(System.out::println);
+    }
+
+    private static void testToCollection(){
+        Optional.of( menu.stream().filter(d -> d.getCalories() > 600).collect(Collectors.toCollection(LinkedList::new)))
+                .ifPresent(System.out::println);
+    }
+
+    private static void testToConcurrentMap(){
+        Optional.of( menu.stream()
+                .collect(Collectors.toConcurrentMap(Dish::getName, Dish::getCalories)))
+                .ifPresent((v) -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+    }
+
+    // Type : Total
+    //The ConcurrentHashMap is thread safe but not ordered
+    private static void testToConcurrentMapWithBinaryOperator(){
+        Optional.of( menu.stream()
+                .collect(Collectors.toConcurrentMap(Dish::getType, v -> 1L, (a, b) -> a + b)))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+    }
+
+    private static void testToConcurrentMapWithBinaryOperatorAndSupplier(){
+        Optional.of( menu.stream()
+                .collect(Collectors.toConcurrentMap(Dish::getType, v -> 1L, (a, b) -> a +b, ConcurrentSkipListMap::new)))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+    }
+
+    private static void testToList(){
+        Optional.of( menu.stream().filter(Dish::isVegetarian).collect(Collectors.toList()))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+    }
+
+    // no doublons
+    private static void testToSet(){
+        Optional.of( menu.stream().filter(Dish::isVegetarian).collect(Collectors.toSet()))
+                .ifPresent( v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+    }
+
+    private static void testToMap(){
+        Optional.of( menu.stream()
+                .collect(Collectors.toMap(Dish::getName, Dish::getCalories)))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+
+        //thread safe
+        Optional.of( menu.stream()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toMap(Dish::getName, Dish::getCalories),
+                        Collections::synchronizedMap)))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+
+    }
+
+    private static void testToMapWithBinaryOperator(){
+        Optional.of( menu.stream()
+                .collect(Collectors.toMap(Dish::getType, v -> 1L, (a, b) -> a + b)))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
+    }
+
+    private static void testToMapWithBinaryOperatorAndSupplier(){
+        Optional.of( menu.stream()
+                .collect(Collectors.toMap(Dish::getType, v -> 1L, (a, b) -> a +b, Hashtable::new)))
+                .ifPresent(v -> {
+                    System.out.println(v.getClass());
+                    System.out.println(v);
+                });
     }
 }
